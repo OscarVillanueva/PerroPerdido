a07c15f4ca0ff0750e235a6b3adb0c7924bf3304
Oscar Javier Villanueva Prieto <javiondo68@gmail.com>
2018-11-25 23:01:04 -0600

    Control de versiones de Perro Perdido

---
+++ app/src/main/java/com/example/oscarvillanuva/perroperdido/Foto.java
@@ -0,0 +1,13 @@
+package com.example.oscarvillanuva.perroperdido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Foto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foto);
+    }
+}

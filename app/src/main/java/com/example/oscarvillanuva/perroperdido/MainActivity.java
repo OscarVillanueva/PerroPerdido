78c68b4b89fce23c7e1161509b1d52ba635b3d11
RazielO <oraziel_a@hotmail.com>
2018-11-26 19:56:35 -0600

    Metodo para tomar fotografia

--- app/src/main/java/com/example/oscarvillanuva/perroperdido/MainActivity.java
+++ app/src/main/java/com/example/oscarvillanuva/perroperdido/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.oscarvillanuva.perroperdido;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Bitmap picture;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void takePicture()
+    {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK)
+            picture = (Bitmap) data.getExtras().get("data");
     }
 }
